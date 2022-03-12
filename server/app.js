e46dcf9e597777081430fcbe5284560130bc98c8
Hani <heenoow@gmail.com>
2022-03-12 20:07:45 +0200

    styled homepage with static data Relates #3

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const { port } = require('../config');
 const app = express();
 app.set('PORT', port || 1000);
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(join(__dirname, '..', 'public')));
+app.use(express.static(join(__dirname, '..', 'public')));
 app.use(router);
 
 module.exports = app;
