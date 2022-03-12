88b88b486a6fde215e8cad8cf53e8a2b1a32bcd1
Hani <heenoow@gmail.com>
2022-03-13 00:34:31 +0200

    render static posts Relates #3

--- public/home.js
+++ public/home.js
@@ -1,28 +1,33 @@
-/* eslint-disable max-len */
-/* eslint-disable no-lone-blocks */
-const createElement = (tag, classname, parent) => {
-  const item = document.createElement(tag);
-  item.className = classname;
-  parent.appendChild(item);
-  return item;
-};
-const homeContent = document.querySelector('.home_content');
 for (let index = 0; index < 5; index++) {
-  let post = createElement('div', 'user_post', homeContent);
-  console.log(post);
+  const homeContent = document.querySelector('.home_content');
+  const post = document.createElement('div');
+  post.className = 'user_post';
+  const postInfo = document.createElement('div');
+  postInfo.className = 'post_info';
+  const avatar = document.createElement('img');
+  avatar.className = 'post_avatar';
+  avatar.src = 'images/avatar.png';
+  avatar.alt = 'POST AVATAR HERE';
+  const username = document.createElement('small');
+  username.className = 'post_username';
+  username.textContent = 'u/heenoow';
+  const postDate = document.createElement('small');
+  postDate.className = 'post_date';
+  postDate.textContent = '56 min ago';
+  const postBody = document.createElement('div');
+  postBody.className = 'post_body';
+  const postTitle = document.createElement('h3');
+  postTitle.textContent =
+    'When I was a kid, me and my older brother smashed the windows to a car.';
+  const postDesc = document.createElement('p');
+  postDesc.textContent =
+    ' I cant really remember how old we were. I was probably around 6-7. My brother was 8-9. (Lets call him jay). And we decided to throw rocks and bash the windows of a car. The car was an old family van, and it was abandoned. It hadnt seen use in years.';
+  postInfo.appendChild(avatar);
+  postInfo.appendChild(username);
+  postInfo.appendChild(postDate);
+  post.appendChild(postInfo);
+  postBody.appendChild(postTitle);
+  postBody.appendChild(postDesc);
+  post.appendChild(postBody);
+  homeContent.appendChild(post);
 }
-
-/* <div class="user_post">
-            <div class="post_info">
-                <img class="post_avatar" src="images/avatar.png" alt="POST AVATAR HERE" height="20px">
-                <small class="post_username">u/heenoow</small>
-                <small class="post_date">56 min ago</small>
-            </div>
-            <div class="post_body">
-                <h3>When I was a kid, me and my older brother smashed the windows to a car.</h3>
-                <p>
-                    I can’t really remember how old we were. I was probably around 6-7. My brother was 8-9. (Lets call
-                    him jay). And we decided to throw rocks and bash the windows of a car. The car was an old family
-                    van, and it was abandoned. It hadn’t seen use in years. </p>
-            </div>
-        </div> */
