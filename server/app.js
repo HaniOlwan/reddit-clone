e3067871f8220338e7d7aa4a277ac71c69e32934
Hani <heenoow@gmail.com>
2022-03-13 12:47:33 +0200

    register pages added styles Relates #6

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const { join } = require('path');
+const path = require('path');
 const express = require('express');
 const router = require('./routes');
 const { port } = require('../config');
@@ -6,7 +6,9 @@ const { port } = require('../config');
 const app = express();
 app.use(express.json());
 app.set('PORT', port || 1000);
-app.use(express.static(join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+// app.use(express.static(path.join(__dirname, '..', 'public', 'stylesheets')));
+
 app.use(router);
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,11 @@
+const path = require('path');
+const fs = require('fs');
 const router = require('express').Router();
 const { homeController, postController } = require('./../controllers');
 
+router.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'public', 'pages', 'signup.html'));
+});
 router.get('/api/v1/posts', homeController);
 router.post('/api/v1/post', postController);
 module.exports = router;
