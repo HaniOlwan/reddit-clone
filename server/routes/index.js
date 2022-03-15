f10ff2976511ccf88dbab8ea904fd9a2f2250b61
Hani <heenoow@gmail.com>
2022-03-15 17:53:07 +0200

    sent post data from input to server Relates #19

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,8 +10,8 @@ router.get('/signup', page.signupPage);
 router.post('/api/v1/signup', signupController);
 router.post('/api/v1/user/logout', logoutUser);
 router.get('/', ensureAuthenticated, page.homePage);
-router.get('/api/v1/posts', homeController);
-router.get('/post', page.createPostPage);
+router.get('/api/v1/posts', homeController); // test
+router.get('/post', ensureAuthenticated, page.createPostPage);
 router.post('/api/v1/post', postController);
 
 module.exports = router;
