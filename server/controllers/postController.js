45503f8f2a059806347d1776d15042189eb0792f
Hani <heenoow@gmail.com>
2022-03-15 21:39:11 +0200

    user adds post Relates #19

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,15 +1,16 @@
 const jwt = require('jsonwebtoken');
-const createPost = require('../database/queries/user/userQuery');
+const { createPost } = require('../database/queries/user/userQuery');
 const { ACCESS_TOKEN_KEY } = require('../../config');
-
-const postController = (req, res) => {
+const { handleError } = require('./../utils/helpers');
+const postController = (req, res, next) => {
   const token = req.headers.cookie.split('=')[1];
   jwt.verify(token, ACCESS_TOKEN_KEY, (err, user) => {
-    if (err) return console.log(err);
-
+    if (err) return handleError({ msg: "User isn't verified", status: 401 });
     const { title, body } = req.body;
     const { id } = user;
-    createPost(title, body, id).then(console.log).catch(console.log);
+    createPost(title, body, id)
+      .then(() => res.status(200).json({ msg: 'Post created' }))
+      .catch(next);
   });
 };
 
--- server/database/queries/user/userQuery.js
+++ server/database/queries/user/userQuery.js
@@ -13,9 +13,9 @@ const registerUser = (name, email, password) => {
 };
 
 const createPost = (title, body, userId) => {
-  const sql = 'INSERT INTO posts (title,body,user_id) VALUES ($1,$2,$3) RETURNING *;';
+  const sql = 'INSERT INTO posts (title,body,user_id) VALUES ($1,$2,$3);';
   const values = [title, body, userId];
   return connection.query(sql, values);
 };
 
-module.exports = { registerUser, findUser, createPost };
+module.exports = { findUser, registerUser, createPost };
