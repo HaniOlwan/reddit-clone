2cc7c373cb785593667b0b9c33f812a2be5f43a1
Hani <heenoow@gmail.com>
2022-03-15 23:34:43 +0200

    lazy load to users posts Relates #22

--- server/database/queries/postsQuery.js
+++ server/database/queries/postsQuery.js
@@ -1,6 +1,13 @@
-const connection = require('./../config/connection');
+const connection = require('../config/connection');
 
-module.exports = () =>
-  connection.query(
-    'SELECT * FROM posts as p JOIN users as u ON (p.user_id=u.id) ORDER BY p.id DESC;'
+const renderPosts = (query) => {
+  let limit = 5;
+  let offset = query.page ? query.page * 5 : 5;
+
+  return connection.query(
+    'SELECT p.id, p.title, p.body, u.name FROM posts as p JOIN users as u ON (p.user_id=u.id) ORDER BY p.id DESC LIMIT $1 OFFSET $2;',
+    [limit, offset]
   );
+};
+
+module.exports = renderPosts;
