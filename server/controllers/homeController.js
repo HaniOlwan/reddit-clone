a9794637ccc4844704389a1d0520ca9d73b6f17f
Hani <heenoow@gmail.com>
2022-03-16 21:57:20 +0200

    fix render posts homepage and pagination and add post users Relates #30

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,4 +1,4 @@
-const renderPosts = require('../database/queries/post/postsQueries');
+const renderPosts = require('../database/queries/post/postsQuery');
 
 const homeController = (req, res) =>
   renderPosts(req.query).then((posts) => res.json(posts.rows));
