bd448cd6a14f7ac9589fd8c9b5dbfd04df976133
Hani <heenoow@gmail.com>
2022-03-16 10:02:40 +0200

    created login function Relates #24

--- server/controllers/validation/signinController.js
+++ server/controllers/validation/signinController.js
@@ -1,25 +1,24 @@
 const bcrypt = require('bcryptjs');
 const { getUser } = require('../../database/queries/user/userQuery');
-// const { hashPassword, createUserToken } = require('../../utils/helpers');
+const { hashPassword, createUserToken } = require('../../utils/helpers');
 
 const signinController = (req, res) => {
   const { email, password } = req.body;
 
   getUser(email)
     .then((users) => users.rows)
-    .then((user) =>
-    //   bcrypt.compare(
-    //     password,
-    //     user.password,
-    //     (err, result) =>
-    //       new Promise((resolve, reject) => {
-    //         if (err) return reject(err);
-
-    //         console.log(result);
-    //         resolve(result);
-    //       })
-    //   )
-    );
+    .then(
+      (user) =>
+        new Promise((resolve, reject) => {
+          bcrypt.compare(password, user[0].password, (err, match) => {
+            if (match) res.cookie('token', createUserToken(user[0]));
+            else {
+              reject(err);
+            }
+          });
+        })
+    )
+    .catch('User doesnt exist');
 };
 
 module.exports = signinController;
