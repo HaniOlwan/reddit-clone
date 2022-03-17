0a59ec986608ceb412522e7a77de866e620dde37
Hani <heenoow@gmail.com>
2022-03-17 18:44:48 +0200

    fix login and sign page

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -7,7 +7,8 @@ signupForm.addEventListener('submit', (e) => {
     email: e.target.email.value,
     password: e.target.password.value,
   };
-  fetchData('/api/v1/signup', 'POST', data)
-    .then((response) => response.json())
-    .then((result) => (window.location.href = '/'));
+  fetchData('/api/v1/signup', 'POST', data).then((response) => {
+    if (response.status === 201) {
+      window.location.href = '/';
+  });
 });
