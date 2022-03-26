85b9d80772461725d3bb33d7e4fb9165b60aef3e
HaniOlwan <heenoow@gmail.com>
2022-03-26 12:33:36 +0300

    fix heroku

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,28 +1,20 @@
+require('dotenv').config();
 const { Pool } = require('pg');
-const { DEV_DB_URL } = require('./../../../config');
 
-// let dbURL = '';
+let dbURL = '';
 
-// let { NODE_ENV, DEV_DB_URL, DATABASE_URL, TEST_DB_URL } = process.env;
-// console.log(NODE_ENV);
+let { NODE_ENV, DEV_DB_URL, DATABASE_URL, TEST_DB_URL } = process.env;
 
-// if (NODE_ENV === 'dev') dbURL = DEV_DB_URL;
-// if (NODE_ENV === 'test') dbURL = TEST_DB_URL;
-// if (NODE_ENV === 'production') dbURL = DATABASE_URL;
-// else {
-//   throw new Error('Invalid DB_URL');
-// }
-const database = {
-  host: 'localhost',
-  port: '5432',
-  user: 'postgres',
-  password: 'qwerty',
-  database: 'reddit',
+if (NODE_ENV === 'dev'){ dbURL = DEV_DB_URL;}
+else if (NODE_ENV === 'test') {dbURL = TEST_DB_URL;}
+else if (NODE_ENV === 'production'){ dbURL = DATABASE_URL;}
+else {
+  throw new Error('Invalid DB_URL');
+}
+
+const options = {
+  connectionString: dbURL,
+  ssl: false,
 };
 
-// const options = {
-//   connectionString: DEV_DB_URL,
-//   ssl: false,
-// };
-
-module.exports = new Pool(database);
+module.exports = new Pool(options);
