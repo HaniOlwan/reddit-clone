8fec6b2695d93746349b4dab7ac9573db1908750
HaniOlwan <heenoow@gmail.com>
2022-06-21 21:39:18 +0300

    up

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -13,7 +13,7 @@ if (NODE_ENV === 'test') {
   sslConnection = false;
 } else if (NODE_ENV === 'production') {
   dbUrl = DATABASE_URL;
-  sslConnection = { rejectUnauthorized: true };
+  sslConnection = { rejectUnauthorized: false };
 } else if (NODE_ENV === 'dev') {
   dbUrl = DEV_DB_URL;
   sslConnection = false;
