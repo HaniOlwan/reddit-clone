3a13a63ed65c8faac1e07fc82434cecd5e8d95e3
HaniOlwan <heenoow@gmail.com>
2022-06-21 16:03:18 +0300

    fix ssl connection

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,21 +1,30 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-let dbURL = '';
-
 const {
-  NODE_ENV, DEV_DB_URL, DATABASE_URL, TEST_DB_URL,
+  NODE_ENV, DEV_DB_URL, TEST_DB_URL, DATABASE_URL,
 } = process.env;
 
-if (NODE_ENV === 'dev') { dbURL = DEV_DB_URL; } else if (NODE_ENV === 'test') { dbURL = TEST_DB_URL; } else if (NODE_ENV === 'production') { dbURL = DATABASE_URL; } else {
-  throw new Error('Invalid DB_URL');
+let dbUrl = '';
+let sslConnection = false;
+
+if (NODE_ENV === 'test') {
+  dbUrl = TEST_DB_URL;
+  sslConnection = false;
+} else if (NODE_ENV === 'production') {
+  dbUrl = DATABASE_URL;
+  sslConnection = { rejectUnauthorized: true };
+} else if (NODE_ENV === 'dev') {
+  dbUrl = DEV_DB_URL;
+  sslConnection = false;
+} else {
+  throw new Error('No environment found');
 }
+if (!dbUrl) throw new Error('Invalid db url');
 
 const options = {
-  connectionString: dbURL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: dbUrl,
+  ssl: sslConnection,
 };
 
 module.exports = new Pool(options);
